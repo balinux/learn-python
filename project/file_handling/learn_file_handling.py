f51c887e71777f42b165c554908586a5fdae4cee
balinux <ryojuniyantara@gmail.com>
2022-03-06 08:26:33 +0800

    udpate: documentation

--- project/file_handling/learn_file_handling.py
+++ project/file_handling/learn_file_handling.py
@@ -1,4 +1,6 @@
-# from importlib.resources import path
+# documentation
+# https://www.geeksforgeeks.org/file-handling-python/
+# https://stackoverflow.com/questions/12201928/open-gives-filenotfounderror-ioerror-errno-2-no-such-file-or-directory
 
 import os
 from pathlib import Path
