4f3eb53759b757e70de2e993a8edf7934a184979
balinux <ryojuniyantara@gmail.com>
2022-03-10 07:36:36 +0800

    update: automatic share file

--- project/file_handling/autoamtic_move_file/list_file.py
+++ project/file_handling/autoamtic_move_file/list_file.py
@@ -1,6 +1,8 @@
 from fileinput import filename
 import os
 from pathlib import Path
+import schedule
+import time
 
 path_dir = str(Path(__file__).absolute().parent)
 
@@ -12,18 +14,29 @@ list_of_file = []
 # extention digunakan untuk menentukan penempatan file sesuai folder
 def movingFile(ext, file_path, file):
     if ext == '.jpg':
+        print("Moving Image!!!")
         path_target = str(Path(__file__).absolute().parent) + "/image/" + file
         os.rename(file_path, path_target)
     elif ext =='.txt':
-        print("ini text")
+        print("Moving Text!!!")
         path_target = str(Path(__file__).absolute().parent) + "/text/" + file
         os.rename(file_path, path_target)
+    else:
+        print("Nothing To Do")
 
-for root, dirs, files in os.walk(path_dir):
-    for file in files:
-        filename, file_ext = os.path.splitext(file)
-        #  print(file_ext)
-        # print(os.path.join(root, file))
-        movingFile(file_ext, os.path.join(root, file), file)
+def runningMover():
+    for root, dirs, files in os.walk(path_dir):
+        for file in files:
+            filename, file_ext = os.path.splitext(file)
+            #  print(file_ext)
+            # print(os.path.join(root, file))
+            movingFile(file_ext, os.path.join(root, file), file)
 
+# def job():
+#     runningMover
 
+schedule.every(10).seconds.do(runningMover)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
