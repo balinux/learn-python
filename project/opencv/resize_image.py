504a4d19f27f10c2f1741e232eeae75fa82d5e67
balinux <ryojuniyantara@gmail.com>
2022-03-09 15:17:03 +0800

    update : resized image

---
+++ project/opencv/resize_image.py
@@ -0,0 +1,21 @@
+import cv2
+
+img = cv2.imread('grey.jpg')
+print("original Dimension: ", img.shape)
+
+# menurunkan resize ke 60%
+scale_percent = 30
+width = int(img.shape[1] * scale_percent/100)
+height = int(img.shape[0] * scale_percent/100)
+dim = (width, height)
+
+# resize image
+resized = cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
+
+print('Resized Dimensions : ',resized.shape)
+
+cv2.imshow("resied", resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# referensi: https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
