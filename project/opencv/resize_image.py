f920823f82345885bc7fedf04b49c2210c59a69e
balinux <ryojuniyantara@gmail.com>
2022-03-09 15:33:50 +0800

    update: save opencv

--- project/opencv/resize_image.py
+++ project/opencv/resize_image.py
@@ -14,16 +14,26 @@ resized = cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+# menambah text di image yang di resize
 cv2.putText(resized, 'Balinux', (10, 200), font, 2, (255, 255, 255), 5, cv2.LINE_AA)
 
 cv2.putText(resized, 'Ganteng', (10, 500), font, 2, (255, 255, 255), 5, cv2.LINE_AA)
 
-print('Line AA : ',cv2.LINE_AA)
-
 print('Resized Dimensions : ',resized.shape)
 
 cv2.imshow("resied", resized)
-cv2.waitKey(0)
+
+# menyyimpan gambar yang sudah di custom
+key = cv2.waitKey(0) # menunggu untuk proses selanjutnya
+
+if key == 27: # menunggu untuk tombol ESC
+    cv2.destroyAllWindows()
+
+elif key == ord('s'): # menunggu tombol 'S' di tekan dan melakukan proses simpan gambar
+    cv2.imwrite('gambarbaru.jpg', resized)
+    cv2.destroyAllWindows()
+
+
 cv2.destroyAllWindows()
 
 # referensi: https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
