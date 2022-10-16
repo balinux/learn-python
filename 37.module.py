01b768f1620f57033d26ca5b12a56a89f1388018
balinux <ryojuniyantara@gmail.com>
2022-10-16 09:42:22 +0800

    add: learn module

---
+++ 37.module.py
@@ -0,0 +1,12 @@
+import function_for_module as fff
+
+# cara ke 2
+from function_for_module import say_hello
+
+hello = say_hello("balinux")
+
+total = fff.total(1,2,3,4,5)
+
+print(total)
+print(hello)
+
